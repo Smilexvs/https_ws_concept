61a1bfef1e0b0fa4babb1a5fc687625fb89131fe
Smilexvs <98544457+Smilexvs@users.noreply.github.com>
2023-09-04 17:14:35 -0300

    CodeFormat added\r\nCustom Partition added

--- main/main.c
+++ main/main.c
@@ -2,10 +2,55 @@
 #include <stdbool.h>
 #include <unistd.h>
 
-void app_main(void)
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_spiffs.h"
+
+static const char *TAG = "https-wss";
+
+void app_main( void )
 {
-    while (true) {
-        printf("Hello from app_main!\n");
-        sleep(1);
-    }
+	esp_vfs_spiffs_conf_t conf =
+	{ .base_path = "/spiffs", .partition_label = NULL, .max_files = 30, .format_if_mount_failed = false };
+
+	// Use settings defined above to initialize and mount SPIFFS filesystem.
+	// Note: esp_vfs_spiffs_register is an all-in-one convenience function.
+	esp_err_t ret = esp_vfs_spiffs_register( &conf );
+
+	if( ret != ESP_OK )
+	{
+		if( ret == ESP_FAIL )
+		{
+			ESP_LOGE( TAG, "Failed to mount or format filesystem" );
+		}
+		else if( ret == ESP_ERR_NOT_FOUND )
+		{
+			ESP_LOGE( TAG, "Failed to find SPIFFS partition" );
+		}
+		else
+		{
+			ESP_LOGE( TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name( ret ) );
+		}
+		return;
+	}
+
+	size_t total = 0, used = 0;
+	ret = esp_spiffs_info( NULL, &total, &used );
+	if( ret != ESP_OK )
+	{
+		ESP_LOGE( TAG, "Failed to get SPIFFS partition information (%s)", esp_err_to_name( ret ) );
+	}
+	else
+	{
+		ESP_LOGI( TAG, "Partition size: total: %d, used: %d", total, used );
+	}
+
+
+
+
+	while( true )
+	{
+		printf( "Hello from app_main!\n" );
+		sleep( 1 );
+	}
 }
